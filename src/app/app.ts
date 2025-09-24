626ff5294c8919766c7e0b703e84d65b06da2c66
Halvor Njåstad <halvor@njastad.no>
2025-09-24 17:13:56 +0200

    really basic rendering of a fullscreen OSM map, and started trying to fetch amenities with 'bar'-tag. Bounds for fetch-area are currently hardcoded, and will need to be got from map? or leave as certain distance from user for mvp.
    TODO
    decide if fetch-bounds around user are enough for mvp
    union fetch on bar|pub|biergarten|nightclub[...?]
    set 'out' for fetch to be something usefull (could probably keep ids, but figure out a use for them)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Barcard } from './barcard/barcard';
+import { LeafletDirective } from '@bluehalo/ngx-leaflet';
+import * as Leaflet from 'leaflet';
+import type { OverpassJson } from "overpass-ts";
+import { overpass } from 'overpass-ts';
+
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, Barcard, LeafletDirective],
+    templateUrl: './app.html',
+    styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('BarCompany');
+    protected readonly title = signal('BarCompany');
+
+    options: Leaflet.MapOptions = {
+        layers: [
+            Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&amp;copy; OpenStreetMap contributors'
+            })
+        ],
+        zoom: 15,
+        center: new Leaflet.LatLng(59.912527852972985, 10.746832664717447)
+    };
+
+
+    //overpass api (used for hitting openstreetmap)
+    request: Promise<Response | void> = overpass(`[out:json]; node["amenity"="bar"](59.9,10.7,60.0,10.8 ); out ids;`)
+        .then((res: Response) => {
+            if (!res.ok) {
+                throw new ReferenceError("todo exceptions :)");
+            }// res ok
+            res.json().then((val: OverpassJson) => {
+                console.log(val);
+            });
+        });
+
 }
---
+++ src/app/barcard/barcard.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+//learning about components. Thinking this could be a card-style popup when user clicks on a bar on the map?
+@Component({
+    selector: 'app-barcard',
+    imports: [],
+    templateUrl: './barcard.html',
+    styleUrl: './barcard.scss'
+})
+export class Barcard {
+
+}
